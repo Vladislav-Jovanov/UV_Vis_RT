c24ec19e9bd8efd0f766c7c173f5e0910f250dcd
Vladislav Jovanov <vladislav.jovanov@ihtm.bg.ac.rs>
2025-08-28 21:44:10 +0200

    submodule update and old RT is now moved to old folder

--- run_app.py
+++ run_app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from GUIs.tot_RT.tot_RT import E60_tot_RT
-from GUIs.tot_RT_new.tot_RT import E60_tot_RT as E60_tot_RT_new
+#from GUIs.tot_RT_old.tot_RT import E60_tot_RT
+from GUIs.tot_RT.tot_RT import E60_tot_RT as E60_tot_RT
 from GUIs.plot_RTA.plot_RTA import plot_RTA
 from GUIs.RTA_GUI.RTA import calc_A
 from AppHub.Hub import MultipleApps
 
-MultipleApps(app_list={'Process raw\ndata new':E60_tot_RT_new, 'Process raw data':E60_tot_RT, 'Calculate A': calc_A, 'plot RTA data':plot_RTA}).init_start()
+MultipleApps(app_list={'Process raw\ndata':E60_tot_RT, 'Calculate A': calc_A, 'plot RTA data':plot_RTA}).init_start()
