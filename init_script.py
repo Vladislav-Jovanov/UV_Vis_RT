6404e59db2582636a1c63c186021de1ce1d3e0f9
Vladislav Jovanov <vladislav.jovanov@ihtm.bg.ac.rs>
2025-09-03 13:21:03 +0200

    help script update

--- init_script.py
+++ init_script.py
@@ -8,7 +8,7 @@ Created on Wed Sep  3 09:28:41 2025
 
 import os
 
-dirname=os.path.dirname(__file__)
+dirname=os.path.dirname(os.path.abspath(__file__))
 os.chdir(dirname)
 os.system('git submodule update --init --recursive --remote')
 for item in ['tkWindget', 'RW_data', 'AppHub', 'DataProcess', 'Figures']:
--- update_script.py
+++ update_script.py
@@ -9,7 +9,7 @@ Created on Wed Sep  3 09:28:41 2025
 import os
 
 os.system('git pull origin main')
-dirname=os.path.dirname(__file__)
+dirname=os.path.dirname(os.path.abspath(__file__))
 for item in ['tkWindget', 'RW_data', 'AppHub', 'DataProcess', 'Figures']:
     if os.path.isdir(os.path.join(dirname,item)):
         os.chdir(os.path.join(dirname,item))
