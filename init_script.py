e0fb1274a24c261b7d86f09db772f01c1d26b88a
Vladislav Jovanov <vladislav.jovanov@ihtm.bg.ac.rs>
2025-10-10 13:13:56 +0200

    variables intoduced to init_script to make it easier for future projects

--- init_script.py
+++ init_script.py
@@ -10,24 +10,33 @@ from os import path, chdir, system
 from numpy import savetxt
 from platform import system as psys
 
+linkname='UV_Vis data process'
+pngicon='UV_Vis.png'
+icoicon='UV_Vis.ico'
+linuxname='UV_Vis_App.desktop'
+scriptname='run_app.py'
+windgetlist=['tkWindget', 'RW_data', 'AppHub', 'DataProcess', 'Figures']
+
 dirname=path.dirname(path.abspath(__file__))
 initfinish=path.join(dirname,'init_finished.ini')
 if not path.isfile(initfinish):
     chdir(dirname)
     system('git submodule update --init --recursive --remote')
-    for item in ['tkWindget', 'RW_data', 'AppHub', 'DataProcess', 'Figures']:
+    for item in windgetlist:
         if path.isdir(path.join(dirname,item)):
             chdir(path.join(dirname,item))
             system('git checkout main')
+    if psys()=="Linux":
+        system(f'chmod +x {path.join(dirname,scriptname)}')
     open(initfinish,'w').close()
 
 if psys()=="Linux":
-    linuxtext=['[Desktop Entry]','Encoding=UTF-8','Name=UV_Vis data process','Type=Application',
-          'Exec='+ path.join(dirname,'run_app.py')+' %F',
-          'Icon='+path.join(dirname,'icons','UV_Vis'),
+    linuxtext=['[Desktop Entry]','Encoding=UTF-8','Name='+linkname,'Type=Application',
+          'Exec='+ path.join(dirname,scriptname)+' %F',
+          'Icon='+path.join(dirname,'icons',pngicon),
           'Categories=Development']
     user=path.expanduser('~')
-    desktop=path.join(user,'Desktop','UV_Vis_App.desktop')
+    desktop=path.join(user,'Desktop',linuxname)
     if not path.isfile(desktop):
         with open(desktop,'w') as f:
             for line in linuxtext:
@@ -38,11 +47,11 @@ if psys()=='Windows':
     from sys import executable
 
     desktop = desktop()
-    shortcut_path=path.join(desktop,"UV_Vis data process.lnk")
-    icon_path = path.join(dirname,'icons','UV_Vis.ico')
+    shortcut_path=path.join(desktop,linkname+".lnk")
+    icon_path = path.join(dirname,'icons',icoicon)
 
     target_path = executable.replace('python.exe','pythonw.exe')
-    target_script=path.join(dirname,'run_app.py')
+    target_script=path.join(dirname,scriptname)
 
     if not path.isfile(shortcut_path):
         with shortcut(shortcut_path) as link:
